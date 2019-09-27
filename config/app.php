beb17fccdd3b19075a8eced083638a075bb61929
Mohamed Moho <mohamed.munawwar.luthfee@gmail.com>
2019-09-28 04:59:30 +0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'url' => 'https://linkserveoctober.herokuapp.com/',
+    'url' => 'http://linkserveoctober.test/',
 
     /*
     |--------------------------------------------------------------------------
